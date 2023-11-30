ab1620f73154169c42018b1cc770c164294acdd0
ATesic49 <atesic7@gmail.com>
2023-11-30 18:58:49 +0100

    Feed Doradjen. Pocteak usequeryja

--- app/feed/[slug]/page.tsx
+++ app/feed/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 import Match from '../../components/Feed/Match'
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import axios from 'axios'
 import { useParams } from 'next/navigation'
@@ -10,37 +10,42 @@ import { useParams } from 'next/navigation'
 
 
 interface MathchesProps {
-    pageParam: Number,
     isUpcoming: String
 }
 
 
-const getMatches = async ({ pageParam = 0, isUpcoming }: MathchesProps) => {
+const getMatches = async (isUpcoming: String) => {
     let api_key = ''
     isUpcoming ? (api_key = 'api key za predstojece') : 'api key za prethodne '
-    const res = await axios.get(api_key)
-    const data = res.data()
-    return { ...data, prevOffset: pageParam }
+    const res = await axios.get('https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard')
+    const data = res.data.events
+    return data
 }
 function page() {
     const { slug: isUpcoming } = useParams()
     const isUpcomingBoolean = isUpcoming === 'true'
     console.log(isUpcoming)
-    const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
-        queryKey: ['matches'],
-        queryFn: () => getMatches({ pageParam: 1, isUpcoming: isUpcoming as String }),
-        getNextPageParam: lastPage => {
-            if (lastPage.prevOffset + 10 > lastPage.articleCount) {
-                return false
-            }
+    // const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    //     queryKey: ['matches'],
+    //     queryFn: () => getMatches({ pageParam: 1, isUpcoming: isUpcoming as String }),
+    //     getNextPageParam: lastPage => {
+    //         if (lastPage.prevOffset + 10 > lastPage.articleCount) {
+    //             return false
+    //         }
 
-            return lastPage.prevOffset + 10
-        },
+    //         return lastPage.prevOffset + 10
+    //     },
+    // })
+    // console.log(data)
+    // const articles = data?.pages.reduce((acc: any, page: any) => {
+    //     return [...acc, ...page.articles]
+    // }, [])
+    const { data: utakmice, status, error } = useQuery({
+        queryKey: ['utakmice'],
+        queryFn: () => getMatches(isUpcoming as String)
+
     })
-    const articles = data?.pages.reduce((acc: any, page: any) => {
-        return [...acc, ...page.articles]
-    }, [])
-
+    console.log(utakmice)
     return (
         <div className="w-full flex flex-col justify-center  bg-stone-100 p-5 mb-5">
             <div className="flex justify-center items-center mb-10">
@@ -49,15 +54,23 @@ function page() {
                 </h1>
             </div>
             <div className="flex flex-col justify-center gap-3">
+                {utakmice?.map((utakmica: object) => {
+                    return (
+                        <div>{utakmica.id}</div>
+                    )
+                })}
                 {/* <InfiniteScroll
-                    dataLength={articles ? articles.length : 0}
+                    dataLength={articles ? articles.length : 1}
                     next={() => fetchNextPage()}
                     hasMore={hasNextPage}
                     loading={<div>Loading...</div>}
+
                 >
                     <div>
                         {articles &&
-                            articles.map((data, index) => <Match key={index} data={data} />)}
+                            // articles.map((data, index) => <Match key={index} data={data} />
+                            articles.map((data, index) => <div key={index} >sadas</div>
+                            )}
                     </div>
                 </InfiniteScroll> */}
             </div>
